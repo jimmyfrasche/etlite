0c170bf8841965f5e6df8f290a7878755fb7d8a9
jimmyfrasche <soapboxcicero@gmail.com>
2016-09-08 18:36:08 -0700

    handle escape sequences

--- internal/internal/runefrom/string.go
+++ internal/internal/runefrom/string.go
@@ -4,20 +4,24 @@ package runefrom
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"unicode/utf8"
 )
 
 //String converts a nonempty string to a rune,
 //returning an error if s contains more than one rune or the rune is invalid.
-func String(s string) (rune, error) { //BUG(jmf): should handle \0 \n \t
+func String(s string) (rune, error) {
 	if s == "" {
 		return -1, errors.New("no unicode code point specified")
 	}
-	r, sz := utf8.DecodeRuneInString(s)
+	r, _, t, err := strconv.UnquoteChar(s, 0)
+	if err != nil {
+		return -1, err
+	}
 	if r == utf8.RuneError {
 		return -1, errors.New("invalid unicode code point")
 	}
-	if s[sz:] != "" {
+	if len(t) > 0 {
 		return -1, fmt.Errorf("expected a single unicode code point, got %q", s)
 	}
 	return r, nil
