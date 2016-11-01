3cee8debbba9c5ddb572d37b8168e6bcf7209e12
jimmyfrasche <soapboxcicero@gmail.com>
2016-11-01 16:58:35 -0700

    add interface for getting token position

--- internal/token/pos.go
+++ internal/token/pos.go
@@ -2,6 +2,11 @@ package token
 
 import "fmt"
 
+//Posers can report their Position.
+type Poser interface {
+	Pos() Position
+}
+
 //Position of a token in input.
 type Position struct {
 	Name       string
@@ -10,4 +15,8 @@ type Position struct {
 
 func (p Position) String() string {
 	return fmt.Sprintf("%s:%d:%d", p.Name, p.Line, p.Rune)
+}
+
+func (p Position) Pos() Position {
+	return p
 }
